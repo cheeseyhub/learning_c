9dbab601b430bc5dcdb47276050209535300f067
CheeseyHub <muhammedibrahim613@gmail.com>
2025-10-15 07:24:23 +0500

    Smooth slow movement

--- game_c/test.c
+++ game_c/test.c
@@ -1,72 +1,41 @@
+#include "math.h"
 #include "raylib.h"
 
-// Structure of Car
-
-struct Car {
-  int posX;
-  int posY;
-  int width;
-  int height;
-  Color color;
-  int velocity;
-  int up;
-  int down;
-  int left;
-  int right;
-};
-void Carcontrols(struct Car *o) {
-  o->up = IsKeyDown(KEY_UP);
-  o->down = IsKeyDown(KEY_DOWN);
-  o->left = IsKeyDown(KEY_LEFT);
-
-  o->right = IsKeyDown(KEY_RIGHT);
-}
-void Carmovement(struct Car *o) {
-  if (o->up) {
-    o->posY -= o->velocity;
-  }
-  if (o->down) {
-    o->posY += o->velocity;
-  }
-  if (o->left) {
-    o->posX -= o->velocity;
-  }
-  if (o->right) {
-    o->posX += o->velocity;
-  }
-}
-
-// Ball  object
-struct ball {
-  int centerX;
-  int centerY;
-  Color color;
-  int radius;
+struct movementComponent {
   float velocity;
   int up;
   int down;
   int left;
   int right;
 };
-void controls(struct ball *o) {
+// Structure of Car
+
+struct Car {
+  float posX;
+  float posY;
+  int width;
+  int height;
+  Color color;
+  struct movementComponent move;
+};
+void GenericControls(struct movementComponent *o) {
   o->up = IsKeyDown(KEY_UP);
   o->down = IsKeyDown(KEY_DOWN);
   o->left = IsKeyDown(KEY_LEFT);
-
   o->right = IsKeyDown(KEY_RIGHT);
 }
-void movement(struct ball *o) {
+void GenericMovement(struct movementComponent *o, float *posX, float *posY) {
   if (o->up) {
-    o->centerY -= o->velocity;
+    *posY -= o->velocity;
   }
   if (o->down) {
-    o->centerY += o->velocity;
+    *posY += o->velocity;
   }
   if (o->left) {
-    o->centerX -= o->velocity;
+    *posX -= o->velocity;
   }
   if (o->right) {
-    o->centerX += o->velocity;
+    *posX += o->velocity;
   }
 }
 
@@ -76,12 +45,13 @@ int main(void) {
 
   InitWindow(windowWidth, windowHeight, "raylib [core] example - basic window");
 
-  struct Car car = {windowWidth / 2, windowHeight / 2, 10, 20, BLACK, 1};
+  struct Car car = {
+      windowWidth / (float)2, windowHeight / (float)2, 10, 20, BLACK, {0.1}};
 
   while (WindowShouldClose() == false) {
 
-    Carcontrols(&car);
-    Carmovement(&car);
+    GenericControls(&car.move);
+    GenericMovement(&car.move, &car.posX, &car.posY);
 
     BeginDrawing();
     ClearBackground(RAYWHITE);
