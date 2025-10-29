0d081fa688dc70117e6b0fb02bc30a4111fb7542
CheeseyHub <muhammedibrahim613@gmail.com>
2025-10-29 11:26:24 +0500

    Did player score implementation

--- pong/pong.c
+++ pong/pong.c
@@ -9,6 +9,7 @@ struct Paddle {
   int width;
   int height;
   double velocity;
+  int score;
 };
 
 struct Ball {
@@ -18,10 +19,10 @@ struct Ball {
   double velocityX;
   double velocityY;
 };
-double stand_random() { return (rand() % 100) + 1; }
+double std_random() { return rand() % (200 - 100 + 1); }
 
 void ballMovement(struct Ball *ball, float deltaTime, int windowWidth,
-                  int windowHeight) {
+                  int windowHeight, int *playerOneScore, int *playerTwoScore) {
 
   if (ball->y + ball->radius > windowHeight || ball->y - ball->radius < 0) {
     ball->velocityY *= -1;
@@ -32,11 +33,25 @@ void ballMovement(struct Ball *ball, float deltaTime, int windowWidth,
   ball->x += velocityX;
   ball->y += velocityY;
 
-  if (ball->x + ball->radius > windowWidth || ball->x - ball->radius < 0) {
+  if (ball->x + ball->radius > windowWidth) {
+
     ball->x = windowWidth / 2.0;
     ball->y = windowHeight / 2.0;
-    ball->velocityX = 200;
-    ball->velocityY = 200;
+
+    ball->velocityX = std_random();
+    ball->velocityY = std_random();
+
+    *playerOneScore = *playerOneScore + 1;
+
+  } else if (ball->x - ball->radius < 0) {
+
+    ball->x = windowWidth / 2.0;
+    ball->y = windowHeight / 2.0;
+
+    ball->velocityX = std_random();
+    ball->velocityY = std_random();
+
+    *playerTwoScore = *playerTwoScore + 1;
   }
 };
 int distance_formula(struct Ball *ball, struct Paddle *paddle) {
@@ -76,7 +91,7 @@ void paddleBallCollision(struct Ball *ball, struct Paddle *paddle,
                          float deltaTime, int windowWidth, int windowHeight) {
 
   if (distance_formula(ball, paddle) == 1) {
-    ball->velocityX *= -1.2;
+    ball->velocityX *= -1.4;
   }
 };
 
@@ -115,7 +130,7 @@ void enemyPaddleMovement(struct Paddle *playerPaddle, float windowWidth,
 }
 
 int main() {
-  srand(time(NULL));
+  srand(time(0));
 
   int windowWidth = 1366;
   int windowHeight = 768;
@@ -126,13 +141,17 @@ int main() {
   InitWindow(windowWidth, windowHeight, "Pong Game");
 
   // Paddle one
-  struct Paddle paddle = {0, 0, paddlesWidth, paddlesHeight};
+  struct Paddle playerOne = {0, 0, paddlesWidth, paddlesHeight};
 
   // Paddle two
-  struct Paddle paddleTwo = {windowWidth - paddlesWidth, 0, paddlesWidth,
+  struct Paddle playerTwo = {windowWidth - paddlesWidth, 0, paddlesWidth,
                              paddlesHeight};
-  paddle.velocity = 300;
-  paddleTwo.velocity = 300;
+  playerOne.velocity = 300;
+  playerTwo.velocity = 300;
+
+  // Scores
+  playerOne.score = 0;
+  playerTwo.score = 0;
 
   // Ball
   struct Ball ball = {windowWidth / 2.0, windowHeight / 2.0, 10};
@@ -144,20 +163,23 @@ int main() {
     float deltaTime = GetFrameTime();
 
     // Ball movement
-    ballMovement(&ball, deltaTime, windowWidth, windowHeight);
+    ballMovement(&ball, deltaTime, windowWidth, windowHeight, &playerOne.score,
+                 &playerTwo.score);
 
     // PLAYER PADDLE MOVEMENT
-    playerPaddleMovement(&paddle, windowWidth, deltaTime);
+    playerPaddleMovement(&playerOne, windowWidth, deltaTime);
 
     // enemyPaddleMovement
-    enemyPaddleMovement(&paddleTwo, windowWidth, deltaTime);
+    enemyPaddleMovement(&playerTwo, windowWidth, deltaTime);
+
     // Paddle limits
-    paddleLimits(&paddle, windowWidth, windowHeight);
-    paddleLimits(&paddleTwo, windowWidth, windowHeight);
+    paddleLimits(&playerOne, windowWidth, windowHeight);
+    paddleLimits(&playerTwo, windowWidth, windowHeight);
 
     // Collision
-    paddleBallCollision(&ball, &paddle, deltaTime, windowWidth, windowHeight);
-    paddleBallCollision(&ball, &paddleTwo, deltaTime, windowWidth,
+    paddleBallCollision(&ball, &playerOne, deltaTime, windowWidth,
+                        windowHeight);
+    paddleBallCollision(&ball, &playerTwo, deltaTime, windowWidth,
                         windowHeight);
     BeginDrawing();
 
@@ -165,15 +187,18 @@ int main() {
     ClearBackground(BLACK);
 
     // Draw Player
-    DrawRectangle(paddle.x, paddle.y, paddle.width, paddle.height, BLUE);
+    DrawRectangle(playerOne.x, playerOne.y, playerOne.width, playerOne.height,
+                  BLUE);
 
     // Draw Enemy;
-    DrawRectangle(paddleTwo.x, paddleTwo.y, paddleTwo.width, paddleTwo.height,
+    DrawRectangle(playerTwo.x, playerTwo.y, playerTwo.width, playerTwo.height,
                   BLUE);
 
     // Draw The ball
     DrawCircle(ball.x, ball.y, ball.radius, WHITE);
 
+    // Drawing Scores
+    DrawText(TextFormat("PlayerOne , %d", playerOne.score), 0, 0, 30, WHITE);
     EndDrawing();
   }
   return 0;
